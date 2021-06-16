19f1b332524033ef410167f8a74b36d3d5cfb0cd
Esther Lecina <estherlecina@gmail.com>
2021-06-16 09:55:37 +0200

    added function test_login_wrong_credentials()

--- ERP_Laravel/tests/Feature/LoginTest.php
+++ ERP_Laravel/tests/Feature/LoginTest.php
@@ -41,6 +41,35 @@ class LoginTest extends TestCase
         $response = $this->post('/login', $credentials);
 
         $response->assertRedirect('/home');
-        $this->assertCredentials($credentials);
+        $this->assertCredentials($credentials)->assertAuthenticated();
+    }
+
+    /**@test */
+    public function test_login_wrong_credentials(){
+
+        $this->withoutExceptionHandling();
+
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+            'type' => 'admin'
+        ]);
+
+        $credentials = [
+            'email' => null,
+            'password' => 'password'
+        ];
+        
+        /**LO QUE DEBERÍA SER.--- LLAMANDO A LA FUNCIÓN test_login_requires_email()--- */
+        //$response = $this->from('/login')->post('/login', $credentials);
+        /*
+        $response->assertRedirect('/login')->assertSessionHasErrors([
+            'email' => 'The email field is required.',
+        ]);
+        */
+
+        /**EN LO QUE SE HA QUEDADO D: */
+        $this->assertGuest();//testea que el usuario no está autenticado
     }
 }
