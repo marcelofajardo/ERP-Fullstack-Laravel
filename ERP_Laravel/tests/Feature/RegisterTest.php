6fd9d2c05ae6c957a938b8b05d0c437b184376c4
sans1960 <asanscliment@gmail.com>
2021-06-16 07:45:02 +0200

    RegisterTest

--- ERP_Laravel/tests/Feature/RegisterTest.php
+++ ERP_Laravel/tests/Feature/RegisterTest.php
@@ -2,26 +2,43 @@
 
 namespace Tests\Feature;
 
+
+
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class RegisterTest extends TestCase
 {
     use RefreshDatabase;
+    use WithoutMiddleware;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function register_test()
+    public function test_register_()
     {
-        $name = $this->faker->name();
-        $email = $this->faker->unique()->safeEmail();
-        $password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
-        $address =$this->faker->address();
-        $cif_nif = $this->faker->randomNumber(9);
-        $image =$this->faker->imageUrl();
+
+              $user = [
+                  'name' => 'John Smith',
+                  'email' => 'john.smith@email.com',
+                  'password' => 'password',
+                  'type'  =>'client',
+                  'address'=>'Calle Mayor,67',
+                  'cif_nif'=>'45678902S',
+                  'image'=>'https://icon-icons.com/es/icono/avatar-hombre-retrato/113269'
+              ];
+
+              $response = $this->post('/register', $user);
+              $this->withoutMiddleware();
+              $response->assertRedirect('/home');
 
 
     }
