54c7b4dec68ce06720ddb0e11bb02e6fce02b74f
sans1960 <asanscliment@gmail.com>
2021-06-16 12:16:13 +0200

    Propuesta de Footer

--- ERP_Laravel/resources/views/layouts/app.blade.php
+++ ERP_Laravel/resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Oswald:wght@300;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
     <!-- Styles -->
@@ -106,5 +107,6 @@
             @yield('content')
         </main>
     </div>
+   @include('layouts.footer')
 </body>
 </html>
