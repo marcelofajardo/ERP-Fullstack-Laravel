7a1620a922d5c7ff0e143bcb30239d6698c5ee2e
Alberto Ortega <ortegasanz@gmail.com>
2021-07-07 20:42:03 +0200

    Cart Language Files

---
+++ ERP_Laravel/resources/lang/ca/cart.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Cart Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+return [
+    'cart'   => 'Cistella',
+    'show_cart' => 'Mostrar Cistella',
+    'id' => '#ID',
+    'name' => 'Nom',
+    'price' => 'Preu',
+    'quantity' => 'Quantitat',
+    'attributes' => 'Atributs',
+    'brand' => 'Marca',
+    'action' => 'Acció',
+    'sub_total' => 'Sub Total',
+    'total' => 'Total',
+    'back_product' => 'Torna a Productes',
+    'back_home' => 'Torna a Home',
+    'action_add' => 'Afegir a la Cistella',
+    'action_delete' => 'Esborra',
+    'action_update' => 'Modifica',
+    'action_clear' => 'Neteja Cistella',
+    'action_add_message' => 'Producte afegit a la cistella.',
+];
