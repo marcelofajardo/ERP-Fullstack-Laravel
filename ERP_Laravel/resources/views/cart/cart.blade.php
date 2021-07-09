853dcf395e917bb716ec4f7cecd995486f026b5b
Alberto Ortega <ortegasanz@gmail.com>
2021-07-09 17:43:52 +0200

    Cart as a Widget updated without refresh. New cart view and navbar design.

---
+++ ERP_Laravel/resources/views/cart/cart.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container" style="margin-top: 80px">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">{{ __('cart.home') }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('product.publicIndex') }}">{{ __('cart.products') }}</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{ __('cart.cart') }}</li>
+            </ol>
+        </nav>
+        @if(session()->has('success_msg'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session()->get('success_msg') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="{{ __('cart.close') }}">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+        @endif
+        @if(session()->has('alert_msg'))
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                {{ session()->get('alert_msg') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="{{ __('cart.close') }}">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+        @endif
+        @if(count($errors) > 0)
+            @foreach($errors0>all() as $error)
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ $error }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="{{ __('cart.close') }}">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+            @endforeach
+        @endif
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                <br>
+                @if(\Cart::getTotalQuantity()>0)
+                    <h4>{{ \Cart::getTotalQuantity()}} {{ __('cart.cart_products') }}</h4><br>
+                @else
+                    <h4>{{ __('cart.cart_no_products') }}</h4><br>
+                    <a href="{{ route('product.publicIndex') }}" class="btn btn-dark"> {{ __('cart.continue_shopping') }}</a>
+                @endif
+
+                @foreach($cartCollection as $item)
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <img src="{{ $item->attributes->imgimg }}" class="img-thumbnail" width="200" height="200">
+                        </div>
+                        <div class="col-lg-5">
+                            <p>
+                                <b><a href="/products/{{ $item->id }}">{{ $item->name }}</a></b><br>
+                                <b>{{ __('cart.price') }}: </b>{{ number_format($item->price,2) }} €<br>
+                                <b>{{ __('cart.sub_total') }}: </b>{{ number_format(\Cart::get($item->id)->getPriceSum(),2) }} €<br>
+                                {{--  <b>{{ __('cart.with_discount') }}: </b>{{ number_format(\Cart::get($item->id)->getPriceSumWithConditions(),2) }} --}}
+                            </p>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="row">
+                                <form action="{{ route('cart.destroy', $item->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" value="{{ $item->id }}" id="id" name="id">
+                                    <button class="btn btn-dark btn-sm" style="margin-right: 10px;"><i class="fa fa-trash"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                @endforeach
+                @if(count($cartCollection)>0)
+                    <form action="{{ route('cart.delete') }}" method="POST">
+                        {{ csrf_field() }}
+                        <button class="btn btn-secondary btn-md">{{ __('cart.action_clear') }}</button>
+                    </form>
+                @endif
+            </div>
+            @if(count($cartCollection)>0)
+                <div class="col-lg-5">
+                    <div class="card">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><b>{{ __('cart.total') }}: </b>{{ number_format(\Cart::getTotal(),2) }} €</li>
+                        </ul>
+                    </div>
+                    <br><a href="{{ route('product.publicIndex') }}" class="btn btn-dark">{{ __('cart.continue_shopping') }}</a>
+                    <a href="#" class="btn btn-success">{{ __('cart.proceed_checkout') }}</a>
+                </div>
+            @endif
+        </div>
+        <br><br>
+    </div>
+@endsection
