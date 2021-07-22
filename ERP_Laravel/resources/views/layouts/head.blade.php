29f0c919430abd99a6aa65e67a88ccf6e69df550
EstherBlackSmith <githubesther@gmail.com>
2021-07-22 13:05:56 +0200

    Created a proposal for new app colors and new file for head html

---
+++ ERP_Laravel/resources/views/layouts/head.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Oswald:wght@300;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Oswald', sans-serif;
+        }
+
+        .logo {
+            font-family: 'Nunito', sans-serif;
+        }
+         
+        body {
+            height:100%;
+            position:relative
+        }
+        .btn-outline-dark:not(:disabled):not(:disabled):active {
+            background: red;
+        } 
+
+    </style>
+</head>
